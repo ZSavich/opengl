8b457abb51f858bf74c1d67dd0a51052e8187e19
PINGLE\e.savych <ye.savych@pinglestudio.com>
2023-06-27 22:53:22 +0300

    Created window

--- Main.cpp
+++ Main.cpp
@@ -9,5 +9,18 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // Inform GLFW that we want use OpenGL x.3
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Inform that we want use Core-Profile
 
+	GLFWwindow* Window = glfwCreateWindow(800, 600, " OpenGL by Savich Eugene", nullptr, nullptr);
+
+	if (!Window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	
+	// Make Window's context as main context for current thread
+	glfwMakeContextCurrent(Window);
+
+	system("pause");
 	return 0;
 }
